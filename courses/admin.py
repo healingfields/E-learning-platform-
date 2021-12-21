4475e3fbfd7b415e17b6faa4b627d1ae14d62ff5
healingfields <dihratene@gmail.com>
2021-12-21 17:48:04 +0100

    created the main models

---
+++ courses/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.db import models
+from .models import Course, Subject, Module
+# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {"slug":('title',)}
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'owner', 'created']
+    prepopulated_fields = {'slug':('title',),}
+    list_filter = ['owner', 'subject', 'created']
+    search_fields = ['title', 'overview']
+    inlines = [ModuleInline ]
