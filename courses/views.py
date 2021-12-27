8eb0d1c060fc2a5a622c900f0e2e0f3fa954f941
healingfields <dihratene@gmail.com>
2021-12-28 00:07:54 +0100

    working on module's content

--- courses/views.py
+++ courses/views.py
@@ -90,7 +90,7 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
         self.module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         self.model = self.get_model(model_name)
         if id:
-            self.obj = get_object_or_404(self.model, id=id, ownner=request.user)
+            self.obj = get_object_or_404(self.model, id=id, owner=request.user)
         return super().dispatch(request, module_id, model_name, id)
 
     def get(self, request, module_id, model_name, id=None):
