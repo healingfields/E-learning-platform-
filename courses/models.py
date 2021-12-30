932d1cd16c32abe146051d6c69b97acb98095976
healingfields <dihratene@gmail.com>
2021-12-30 02:34:42 +0100

    registering students

--- courses/models.py
+++ courses/models.py
@@ -21,12 +21,13 @@ class Course(models.Model):
                             related_name='courses',
                             on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject,
-                            related_name='courses',
-                            on_delete=models.CASCADE)
+                            related_name= 'courses',
+                            on_delete= models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+
 
     class Meta:
         ordering = ['-created']
