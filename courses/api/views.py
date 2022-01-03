1ff7290bd3a84b6a5ce6d9e38921dec7e9d0c6de
healingfields <dihratene@gmail.com>
2022-01-03 01:59:23 +0100

    adding auth and permissions

--- courses/api/views.py
+++ courses/api/views.py
@@ -5,6 +5,8 @@ from .serializers import SubjectSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class SubjectListView(generics.ListAPIView):
   queryset = Subject.objects.all()
@@ -15,6 +17,8 @@ class SubjectDetailView(generics.RetrieveAPIView):
   serializer_class = SubjectSerializer
 
 class CourseEnrollView(APIView):
+  authentication_classes = (BasicAuthentication,)
+  permission_classes = (IsAuthenticated,)
   def post(self, request, pk, format=None):
     course = get_object_or_404(Course, pk=pk)
     course.students.add(request.user)
