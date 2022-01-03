aecd1cb8f71983bad99bb66081502496bffd632e
healingfields <dihratene@gmail.com>
2022-01-03 02:51:29 +0100

    api for showing a specific course whole details(modules,contents)

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from ..models import Subject, Course, Module
+from rest_framework.utils import serializer_helpers
+from ..models import Subject, Course, Module, Content
 
 class SubjectSerializer(serializers.ModelSerializer):
   class Meta:
@@ -16,6 +17,29 @@ class CourseSerializer(serializers.ModelSerializer):
   class Meta:
     model = Course
     fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
+
+class ItemRelatedField(serializers.RelatedField):
+  def to_representation(self, value):
+      return value.render()
+    
+class ContentSerializer(serializers.ModelSerializer):
+  item = ItemRelatedField(read_only=True)
+  class Meta:
+    model = Content
+    fields = ['order', 'item']
+
+class ModuleWithContentSerializer(serializers.ModelSerializer):
+  contents = ContentSerializer(many=True)
+  class Meta:
+    model = Module
+    fields = ['order', 'title', 'description', 'contents']
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+  modules = ModuleWithContentSerializer(many=True)
+  class Meta:
+    model = Course
+    fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
+
   
 
 
--- courses/api/views.py
+++ courses/api/views.py
@@ -2,13 +2,15 @@ from rest_framework import generics
 from rest_framework.decorators import action, authentication_classes, permission_classes
 from rest_framework.serializers import Serializer
 from ..models import Subject, Course
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+from .permissions import IsEnrolled
+
 
 class SubjectListView(generics.ListAPIView):
   queryset = Subject.objects.all()
@@ -38,3 +40,11 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     course = self.get_object()
     course.students.add(request.user)
     return Response({'enrolled':True})
+
+  @action(detail=True,
+          methods=['get'],  
+          serializer_class=CourseWithContentsSerializer,
+          authentication_classes=[BasicAuthentication],
+          permission_classes=[IsAuthenticated, IsEnrolled])
+  def contents(self, request, *args, **kwargs):
+          return self.retrieve(request, *args, **kwargs)
