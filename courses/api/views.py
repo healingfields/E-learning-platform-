34711b44c5d05b03890d1c5d688bdfc9a9d57e7b
healingfields <dihratene@gmail.com>
2022-01-03 01:45:56 +0100

    added enroll course api

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
 from rest_framework.serializers import Serializer
-from ..models import Subject
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class SubjectListView(generics.ListAPIView):
   queryset = Subject.objects.all()
@@ -10,3 +13,9 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
   queryset = Subject.objects.all()
   serializer_class = SubjectSerializer
+
+class CourseEnrollView(APIView):
+  def post(self, request, pk, format=None):
+    course = get_object_or_404(Course, pk=pk)
+    course.students.add(request.user)
+    return Response({'enrolled':True})
